6e0d641eb8d831b746b51178d32d1c75c09a2f04
Kazunori Itoyanagi <itkz@yuiseki.net>
2010-04-12 06:17:28 +0900

    glitch

--- app.rb
+++ app.rb
@@ -1,15 +1,24 @@
 require 'rubygems'
 require 'sinatra'
 require 'erb'
+require 'yaml'
+require 'net/http'
+require 'uri'
 require 'tokyotyrant'
 include TokyoTyrant
 
+require 'glitch'
+
+
+configure do
+  set :settings, YAML.load_file("settings.yaml")
+end
 
 
 helpers do
   def get_recents(limit, offset)
     rdb = RDBTBL::new
-    rdb.open('localhost', 1978)
+    rdb.open(options.settings["tokyotyrant"]["host"].to_s, options.settings["tokyotyrant"]["port"].to_i)
     qry = RDBQRY::new(rdb)
     # アクセス日時順でソート
     qry.setorder('accessed_at', RDBQRY::QONUMDESC)
@@ -23,9 +32,23 @@ end
 
 get '/' do
   # 最新画像URL500件
-  @elements = get_recents(500,0)
+  @elements = get_recents(options.settings["app"]["recents_num"], 0)
   erb :index
 end
 
 
+get '/glitch/*' do
+  url_path = request.fullpath.scan(/^\/glitch\/(https?:\/\/[-_.!~*'()a-zA-Z0-9;\/?:@&=+$,%#]*)$/).flatten!.first
+  url = URI.parse(url_path)
+  request = Net::HTTP::Get.new(url.path)
+  response = Net::HTTP.start(url.host, url.port) do |http|
+    http.request(request)
+  end
+  glitch = Glitch.new(response.body)
+  glitch.break!
+  content_type "image/jpeg"
+  attachment "temp.jpg"
+  glitch.break!
+  return glitch.image
+end
 
