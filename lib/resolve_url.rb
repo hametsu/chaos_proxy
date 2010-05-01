c8318a49ab96083dde030a38f6bb372c1eb88b41
TAKANO Mitsuhiro <takano32@gmail.com>
2010-05-01 18:27:06 +0900

    implemented resovle_shorten_url method

--- lib/resolve_url.rb
+++ lib/resolve_url.rb
@@ -1,6 +1,23 @@
+#! /usr/bin/env ruby
 
+require 'open-uri'
 
-if $0 = __FILE__ then
-	puts "hoge"
+module ShortenUrlResolve
+	def resolve_shorten_url(url)
+		iso = open(url, 'Method' => 'HEAD')
+		return iso.base_uri.to_s
+	end
+	
+	def resolve_tinyurl(url)
+	end
+	
+	def resolve_bitly(url)
+	end
+end
+
+if $0 == __FILE__ then
+	include ShortenUrlResolve
+	puts resolve_shorten_url('http://tinyurl.com/39jo54x')
+	puts resolve_shorten_url('http://bit.ly/bZNHJb')
 end
 
