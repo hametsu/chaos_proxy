fac5178d881f34bb69f9accac2095894b75f345f
yuiseki <yuiseki@gmail.com>
2010-05-07 15:03:22 +0900

    refactoring proxy.rb

--- proxy.rb
+++ proxy.rb
@@ -18,14 +18,14 @@ end
 require 'yaml'
 $settings = YAML.load_file("settings.yaml")
 $allowed_hosts = [
-  "twitter.com", "search.twitter.com",
+  "twitter.com", "m.twitter.com", "mobile.twitter.com", "search.twitter.com",
   "s.twimg.com", "a0.twimg.com", "a1.twimg.com", "a2.twimg.com", "a3.twimg.com", "widgets.twimg.com",
   "www.google.com", "www.google.co.jp", "ajax.googleapis.com",
   "clients1.google.co.jp", "maps.google.com", "maps.gstatic.com", "www.google-analytics.com"
 ]
 $allowed_ctypes = ["image/jpeg", "image/gif", "image/png", nil]
 
-def check_twitter(puid)
+def auth_twitter(puid)
   rdb = RDBTBL::new
   unless rdb.open($settings["twitter"]["host"].to_s, $settings["twitter"]["port"].to_i)
     puts rdb.errmsg(rdb.ecode).red
@@ -37,29 +37,47 @@ def check_twitter(puid)
   if hit.size == 0
     return false
   else
-    puts hit.last.fetch('twitter_name', '!!empty!!').red.bold
-    return true
+    return hit.last.fetch('twitter_name', '!!empty!!')
   end
 end
 
+def regist_twitter(puid, twitter_name)
+  value = {'puid' => puid.to_s, 'twitter_name' => twitter_name.to_s, 'accessed_at' => Time.now.to_i.to_s }
+  rdb = RDBTBL::new
+  rdb.open($settings["twitter"]["host"].to_s, $settings["twitter"]["port"].to_i)
+  key = rdb.rnum + 1
+  rdb.put(key, value)
+  rdb.close
+end
+
 handler = Proc.new() {|req,res|
+  puts ''
   path = req.unparsed_uri
   puid = "#{req.peeraddr[2]}:#{req.peeraddr[3]}"
 
-  unless check_twitter(puid)
+  unless twitter_name = auth_twitter(puid)
     unless $allowed_hosts.include?(req.host)
-      puts req.host.red
+      puts "#{puid} at #{req.host.red}"
       unless $allowed_ctypes.include?(res.header["content-type"])
-        puts "force redirect http://twitter.com/login".bold
+        puts "force redirect twitter login page".bold
         res.status = 302
         res.header["location"] = "http://twitter.com/login"
       end
     else
-      puts req.host.yellow
+      puts "#{puid} as #{'anonymous '.red} at #{req.host.yellow}"
     end
   else
-    puts req.host.green
+    puts "#{puid} as #{twitter_name.red.bold} at #{req.host.green}"
   end
+
+
+  if bauth = req.header.fetch('authorization', false) and req.host == 'twitter.com'
+    puts "twitter basic auth"
+    # twitter.comへのbasic認証だったらぶっこぬき
+    auth = bauth.split(/\s/)
+    puts auth.inspect
+  end
+
 
   case path
   when /^http:\/\/twitter\.com\/$/
@@ -75,20 +93,22 @@ handler = Proc.new() {|req,res|
     doc = Hpricot(utf_body)
     span_me_name = doc.search('span#me_name')
     if span_me_name
-      puts 'trying to get twitter name'.yellow
       twitter_name = span_me_name.inner_html
-      puts twitter_name.red.bold
       unless twitter_name == ""
-        value = {'puid' => puid.to_s, 'twitter_name' => twitter_name.to_s, 'accessed_at' => Time.now.to_i.to_s }
-        rdb = RDBTBL::new
-        rdb.open($settings["twitter"]["host"].to_s, $settings["twitter"]["port"].to_i)
-        key = rdb.rnum + 1
-        rdb.put(key, value)
-        rdb.close
+        puts "regist #{puid} as #{twitter_name.red.bold}"
+        regist_twitter(puid, twitter_name)
       end
     end
     code = Kconv.guess(body)
     res.body = utf_body.kconv(code, Kconv::UTF8)
+
+  when /^http:\/\/mobile\.twitter\.com\/$/
+    body = res.body
+    if res.header["content-encoding"] == "gzip"
+      Zlib::GzipReader.wrap(StringIO.new(res.body)){|gz| body = gz.read}
+      res.header.delete("content-encoding")
+      res.header.delete("content-length")
+    end
 
   when /\.(jpg|gif|png)/
     # 画像だったらTTへ保存
@@ -99,19 +119,25 @@ handler = Proc.new() {|req,res|
     hit = qry.searchget
     if hit.size == 0
       # 初回投入
-      value = {"uri" => path, "accessed_at" => Time.now.to_i.to_s, "count"=>"1"}
+      value = {"uri" => path, "accessed_at" => Time.now.to_i.to_s, "count"=>"1", "puid" =>puid}
       key = rdb.rnum + 1
       rdb.put(key, value)
     else
       # 既出のURL。カウントアップ
       count = hit.last.fetch("count", "1").to_i+1
-      value = {"uri" => path, "accessed_at" => Time.now.to_i.to_s, "count"=>count.to_s}
+      value = {"uri" => path, "accessed_at" => Time.now.to_i.to_s, "count"=>count.to_s, "puid"=>puid}
       key = rdb.rnum + 1
       rdb.put(key, value)
     end
     rdb.close
   end
-  #res.header.each{|k,v| puts "#{k} : #{v}".bold }
+  foo = File.open("tmp/proxy.log", 'a')
+  foo.puts "resquest headers : "
+  req.header.each{|k,v| foo.puts "#{k} : #{v}" }
+  foo.puts "response headers : "
+  res.header.each{|k,v| foo.puts "#{k} : #{v}" }
+  foo.puts ""
+  foo.close
 }
 
 s = WEBrick::HTTPProxyServer.new(
