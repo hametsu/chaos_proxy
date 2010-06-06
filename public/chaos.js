43fb4b16b458ccae2420f81669f08b3b2da489fe
hagino3000 <hagino.3000@gmail.com>
2010-06-06 19:13:45 +0900

    Add queueing module

--- public/chaos.js
+++ public/chaos.js
@@ -111,6 +111,104 @@ Chaos.image = {
   }
 }
 
+
+/**
+ * Queueing asyncronous functions. And calls them syncronous.
+ *
+ * Usage:
+ * var queue = new Chaos.Queue({ name : 'notification' });
+ *
+ * queue.push({
+ *   // latest argument is callback function
+ *   fn : function(a, b, callback) {
+ *     doAsync(function(result) {
+ *       callback(result);
+ *     }
+ *   },
+ *   callback : function(result) {
+ *     print(result)
+ *   }
+ * }
+ *
+ * queue.push({
+ *   ...
+ * });
+ *
+ *
+ */
+Chaos.Queue = function(config) {
+  Chaos.Queue.prototype.initialize.call(this, config);
+}
+
+Chaos.Queue.prototype = {
+  initialize : function(config) {
+    this.name = config.name;
+    this.running = false;
+    this.queue = [];
+  },
+
+  /**
+   * @param (Object, Function) fnConf
+   *
+   * Object properties
+   * @param fn Function (requre)
+   * @param scope Object Scope for fn (default : null)
+   * @param delay Number Delay to Start fn (default : 0)
+   * @param args Array Arguments for fn (default : [])
+   * @param callback Function Callback function (default : null)
+   *
+   */
+  push : function(fnConf) {
+    if (lng.isFunction(fnConf)) {
+      fnConf = { fn : fnConf }
+    }
+
+    this.queue.push({
+      fn : fnConf.fn,
+      scope : fnConf.scope || null,
+      delay : fnConf.delay || 0,
+      args : fnConf.args || [],
+      callback : fnConf.callback
+    });
+    if (!this.running) {
+      this._exec();
+    }
+  },
+
+  _exec : function() {
+    var self = this;
+
+    this.running = true;
+    var q = this.queue.shift();
+    if (q.callback) {
+      q.args.push(createCallback());
+    }
+    setTimeout(function(){
+      q.fn.apply(q.scope, q.args);
+      if (!q.callback) {
+        self._next();
+      }
+    }, q.delay);
+
+    function createCallback() {
+      return function(){
+        self._next();
+        var args = Array.prototype.slice.call(arguments);
+        q.callback.apply(q.scope, args);
+      }
+    }
+  },
+
+  _next : function() {
+    if (this.queue.length > 0) {
+      this._exec();
+    } else {
+      this.running = false;
+    }
+  }
+}
+
+
 /**
  * For common utilities
  */
@@ -122,6 +220,9 @@ var lng = {
       args = Array.prototype.slice.call(arguments);
       fn.apply(thisObj, args);
     }
+  },
+
+  isFunction : function(f) {
+    return typeof(f) == 'function' || f instanceof Function;
   }
 }
-
