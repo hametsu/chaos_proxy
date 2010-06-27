77ba59002a6c185043c5a1b768c70307f37d694e
hagino3000 <hagino.3000@gmail.com>
2010-06-27 17:27:35 +0900

    change search hashtag

--- ui/src/js/chaos.js
+++ ui/src/js/chaos.js
@@ -8,7 +8,7 @@ var SETTINGS = {
   MESSAGE_SPEED : 40,
   MAX_IMAGE_SIZE : 1300,
   BENJO_SERVER_WEBSOCKET_URL : 'ws://chaos.yuiseki.net:4569/',
-  TWITTER_SEARCH_KEYWORD : '#ts6'
+  TWITTER_SEARCH_KEYWORD : '#hametsulo'
 }
 
 var MESSAGES = {
