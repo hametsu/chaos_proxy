4e32c704a872d1c1aed90bd805d9579d9fb25138
TAKANO Mitsuhiro <takano32@germanium.(none)>
2010-08-08 14:27:32 +0900

    merged files

--- router/restart.sh
+++ router/restart.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+IF=eth2
 #upstream
-ifconfig eth2 up
-ifconfig eth2 192.168.32.1
+#ifconfig eth2 up
+sudo ifconfig $IF 192.168.0.1
 #iptables -t nat -A PREROUTING -i eth0 -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 3128
-iptables -t nat -A PREROUTING -i eth2 -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 3128
+sudo iptables -t nat -A PREROUTING -i $IF -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 3128
 #downstream
 #iptables -t nat -A PREROUTING -i eth2 -p tcp -m tcp --dport 80 -j DNAT --to-destination 192.168.32.1:3128
 
